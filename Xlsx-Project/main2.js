e4c0d815efb4631c0701faa62378b53cd4b8a127
ZhengAu <598359997@qq.com>
2020-03-14 23:15:35 +0800

    read more sheet in one xlsx

---
+++ Xlsx-Project/main2.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const xlsx = require('node-xlsx');
+
+let filePath = path.resolve('./public/json');
+let xlsxData = xlsx.parse('./public/test.xlsx');
+
+function readXlsx() {
+	for (let data of xlsxData) {
+		let fileName = data.name;
+
+		let listName = data.data[0];
+		let sheetData = data.data.slice(1);
+
+		createObj(fileName, listName, sheetData);
+	}
+
+}
+
+function createObj(fileName, listName, sheetData) {
+	let fileData = [];
+	let len = listName.length;
+
+	for (let data of sheetData) {
+		let obj = {};
+		for (let i = 0; i < len; i++) {
+			obj[`${listName[i]}`] = data[i];
+		}
+		fileData.push(obj);
+	}
+	createFile(fileName, fileData);
+	console.log('fileName---', fileName, '\nfileData---', fileData);
+}
+
+function createFile(fileName, fileData) {
+	fs.writeFile(filePath + `/${fileName}.json`, JSON.stringify(fileData), (err, data) => {
+		if (err) {
+			console.log('error', err);
+			return;
+		}
+		console.log(`The file has been saved!`);
+	});
+}
+
+readXlsx();
